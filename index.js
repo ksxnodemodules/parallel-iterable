459ef59b05d5982d5d698dbbfa3520eff37a9b7f
khai96_ <hvksmr1996@gmail.com>
2017-04-07 20:32:08 +0700

    No wrap-around function

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
+'use strict'
 
-((module) => {
-  'use strict'
+var ParallelIterableSuper = require('x-iterable-utils/appx-super-class.js')(build, iterate)
+var {iterable} = Symbol
 
-  var ParallelIterableSuper = require('x-iterable-utils/appx-super-class.js')(build, iterate)
-  var {iterable} = Symbol
-
-  class ParallelIterable extends ParallelIterableSuper {
-    static END_OF_FIRST ([{done}]) {
-      return done
-    }
-
-    static END_OF_SOME (elements) {
-      return elements.some(({done}) => done)
-    }
-
-    static END_OF_ALL (elements) {
-      return elements.every(({done}) => done)
-    }
-
-    static FOR_COUNT (count) {
-      return (elements) => !(count--)
-    }
-	}
-
-  module.exports = ParallelIterable
-
-  function build (self, stop, ...iterables) {
-    self.stop = stop
-    self.iterables = iterables
+class ParallelIterable extends ParallelIterableSuper {
+  static END_OF_FIRST ([{done}]) {
+    return done
   }
 
-  function * iterate () {
-    var iterators = this.iterables.map((iterable) => iterable[iterable]())
-    for (; ;) {
-      var elements = iterators.map((iterator) => iterator.next())
-      if (this.stop(elements, this)) {
-        break
-      } else {
-        yield elements.map(({value}) => value)
-      }
+  static END_OF_SOME (elements) {
+    return elements.some(({done}) => done)
+  }
+
+  static END_OF_ALL (elements) {
+    return elements.every(({done}) => done)
+  }
+
+  static FOR_COUNT (count) {
+    return (elements) => !(count--)
+  }
+}
+
+module.exports = ParallelIterable
+
+function build (self, stop, ...iterables) {
+  self.stop = stop
+  self.iterables = iterables
+}
+
+function * iterate () {
+  var iterators = this.iterables.map((iterable) => iterable[iterable]())
+  for (; ;) {
+    var elements = iterators.map((iterator) => iterator.next())
+    if (this.stop(elements, this)) {
+      break
+    } else {
+      yield elements.map(({value}) => value)
     }
   }
-})(module)
+}
