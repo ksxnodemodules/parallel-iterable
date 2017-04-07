5922b9ec08372a97a466e2e0ecc9a7a7bba708e1
khai96_ <hvksmr1996@gmail.com>
2017-04-07 20:15:12 +0700

    Run standard --fix

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
 
 ((module) => {
-	'use strict';
+  'use strict'
 
-	var {XIterable} = require('x-iterable-base');
-	var ParallelIterableSuper = require('x-iterable-utils/appx-super-class.js')(build, iterate);
+  var {XIterable} = require('x-iterable-base')
+  var ParallelIterableSuper = require('x-iterable-utils/appx-super-class.js')(build, iterate)
 
-	var _key_iterator = Symbol.iterator;
+  var _key_iterator = Symbol.iterator
 
-	class ParallelIterable extends ParallelIterableSuper {
+  class ParallelIterable extends ParallelIterableSuper {
+    static END_OF_FIRST ([{done}]) {
+      return done
+    }
 
-		static END_OF_FIRST([{done}]) {
-			return done;
-		}
+    static END_OF_SOME (elements) {
+      return elements.some(({done}) => done)
+    }
 
-		static END_OF_SOME(elements) {
-			return elements.some(({done}) => done);
-		}
+    static END_OF_ALL (elements) {
+      return elements.every(({done}) => done)
+    }
 
-		static END_OF_ALL(elements) {
-			return elements.every(({done}) => done);
-		}
-
-		static FOR_COUNT(count) {
-			return (elements) => !(count--);
-		}
-
+    static FOR_COUNT (count) {
+      return (elements) => !(count--)
+    }
 	}
 
-	module.exports = ParallelIterable;
+  module.exports = ParallelIterable
 
-	function build(self, stop, ...iterables) {
-		self.stop = stop;
-		self.iterables = iterables;
-	}
+  function build (self, stop, ...iterables) {
+    self.stop = stop
+    self.iterables = iterables
+  }
 
-	function * iterate() {
-		var iterators = this.iterables.map((iterable) => iterable[_key_iterator]());
-		for ( ; ; ) {
-			var elements = iterators.map((iterator) => iterator.next());
-			if (this.stop(elements, this)) {
-				break;
-			} else {
-				yield elements.map(({value}) => value);
-			}
-		}
-	}
-
-})(module);
+  function * iterate () {
+    var iterators = this.iterables.map((iterable) => iterable[_key_iterator]())
+    for (; ;) {
+      var elements = iterators.map((iterator) => iterator.next())
+      if (this.stop(elements, this)) {
+        break
+      } else {
+        yield elements.map(({value}) => value)
+      }
+    }
+  }
+})(module)
